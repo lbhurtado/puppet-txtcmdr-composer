16640cae9f11d6242b105354c24afee2268d1d41
“Lester <lester@applester.co>
2014-08-22 09:16:56 +0800

    move composer time out to environment
    added —prefer-diet

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,9 +5,9 @@ define composer::install( $path = "/vagrant" ) {
   }
 
   exec { "composer-install":
-    command     => "COMPOSER_PROCESS_TIMEOUT=4000 composer --verbose --no-interaction create-project symfony/framework-standard-edition ./",
+    command     => "composer --prefer-dist --no-interaction create-project symfony/framework-standard-edition ./",
     cwd         => $path,
-    environment => "COMPOSER_HOME=/usr/bin/",
+    environment => ['COMPOSER_HOME=/usr/bin/', 'COMPOSER_PROCESS_TIMEOUT=4000'],
     creates     => "${path}/web",
     timeout     => 0,
     require     => File[ $path ],
